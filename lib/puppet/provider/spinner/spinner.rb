594cf226b4aa96a5d3a406ff077149f21373ed53
Will Meek <william.meek@puppet.com>
2019-04-09 15:47:52 +0100

    (maint) Unit test fixes
    Fixes for the unit tests. This also contains rubocop tidyup.

--- lib/puppet/provider/spinner/spinner.rb
+++ lib/puppet/provider/spinner/spinner.rb
@@ -5,7 +5,7 @@ require 'puppet/util/network_device/spinner/device'
 class Puppet::Provider::Spinner::Spinner < Puppet::ResourceApi::SimpleProvider
   def get(context)
     context.device.spin(context.device.get_cpu_time, context.device.get_wait_time)
-    (0..999).collect do |i|
+    (0..999).map do |i|
       {
         name: i.to_s,
         ensure: 'present',
@@ -14,15 +14,15 @@ class Puppet::Provider::Spinner::Spinner < Puppet::ResourceApi::SimpleProvider
     end
   end
 
-  def create(context, name, should)
+  def create(context, _name, should)
     context.device.spin(should[:cpu_time], should[:wait_time])
   end
 
-  def update(context, name, should)
+  def update(context, _name, should)
     context.device.spin(should[:cpu_time], should[:wait_time])
   end
 
-  def delete(context, name)
+  def delete(_context, name)
     Puppet.info("not spinning on delete of #{name}")
   end
 end
--- spec/unit/puppet/provider/spinner/spinner_spec.rb
+++ spec/unit/puppet/provider/spinner/spinner_spec.rb
@@ -7,43 +7,45 @@ RSpec.describe Puppet::Provider::Spinner::Spinner do
   subject(:provider) { described_class.new }
 
   let(:context) { instance_double('Puppet::ResourceApi::BaseContext', 'context') }
+  let(:device) { instance_double('Puppet::Util::NetworkDevice::Spinner::Device', 'device') }
 
   describe '#get' do
     it 'processes resources' do
-      expect(context).to receive(:debug).with('Returning pre-canned example data')
-      expect(provider.get(context)).to eq [
-        {
-          name: 'foo',
+      expect(context).to receive(:device).and_return(device).exactly(3).times
+      expect(device).to receive(:get_cpu_time).and_return(40)
+      expect(device).to receive(:get_wait_time).and_return(41)
+      expect(device).to receive(:spin).with(40, 41)
+      expected_result = []
+      (0..999).map do |i|
+        expected_result.push(
+          name: i.to_s,
           ensure: 'present',
-        },
-        {
-          name: 'bar',
-          ensure: 'present',
-        },
-      ]
+          trigger: false,
+        )
+      end
+      expect(provider.get(context)).to eq(expected_result)
     end
   end
 
   describe 'create(context, name, should)' do
     it 'creates the resource' do
-      expect(context).to receive(:notice).with(%r{\ACreating 'a'})
-
-      provider.create(context, 'a', name: 'a', ensure: 'present')
+      expect(context).to receive(:device).and_return(device)
+      expect(device).to receive(:spin).with(42, 43)
+      provider.create(context, 'a', name: 'a', cpu_time: 42, wait_time: 43, ensure: 'present')
     end
   end
 
   describe 'update(context, name, should)' do
     it 'updates the resource' do
-      expect(context).to receive(:notice).with(%r{\AUpdating 'foo'})
-
-      provider.update(context, 'foo', name: 'foo', ensure: 'present')
+      expect(context).to receive(:device).and_return(device)
+      expect(device).to receive(:spin).with(42, 43)
+      provider.update(context, 'foo', name: 'foo', cpu_time: 42, wait_time: 43, ensure: 'present')
     end
   end
 
   describe 'delete(context, name)' do
     it 'deletes the resource' do
-      expect(context).to receive(:notice).with(%r{\ADeleting 'foo'})
-
+      expect(Puppet).to receive(:info).with('not spinning on delete of foo')
       provider.delete(context, 'foo')
     end
   end
