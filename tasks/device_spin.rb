a9c0f0d9f44e9b077691b711346ba5ea3138c6cc
Dave Armstrong <david.armstrong@puppet.com>
2019-04-15 13:51:33 +0100

    Removal of file path stuffs (#1)
    * (maint) Add a DeviceSpin task
    
    This will allow for task workflows to be tested without requiring an actual device
    
    * Removal of file path stuffs

--- tasks/device_spin.rb
+++ tasks/device_spin.rb
@@ -1,22 +1,8 @@
 #!/opt/puppetlabs/puppet/bin/ruby
 
 require 'puppet'
-require 'puppet/util/network_device/config'
-
-def require_module_file(file)
-  return unless File.exist?("#{Puppet[:plugindest]}/#{file}.rb")
-  require "#{Puppet[:plugindest]}/#{file}"
-end
-
-installdir_path = '../../ruby_task_helper/files/task_helper.rb'
-local_path = '../files/task_helper.rb'
-
-# Task is being run with bolt and helper is at location relative to task
-if File.exist?(File.join(File.dirname(__FILE__), installdir_path))
-  require_relative installdir_path
-elsif File.exist?(File.join(File.dirname(__FILE__), local_path))
-  require_relative local_path
-end
+require_relative "../../ruby_task_helper/files/task_helper.rb"
+require_relative "../../test_device/lib/puppet/util/network_device/spinner/device.rb"
 
 # validate values.
 def validate_parameters(cpu_time, wait_time)
@@ -60,7 +46,6 @@ class DeviceSpin < TaskHelper
     unless Puppet.settings.global_defaults_initialized?
       Puppet.initialize_settings
     end
-    require_module_file('puppet/util/network_device/spinner/device')
     validate_parameters(params[:_target][:cpu_time], params[:_target][:wait_time])
     spinner_device = Puppet::Util::NetworkDevice::Spinner::Device.new(params[:_target])
     spinner_device.spin(params[:_target][:cpu_time].to_i, params[:_target][:wait_time].to_i)
